4da040af1bd259231fb21b73a9f6a84fff7a8c57
Alex-Lewandowski <aflewandowski@alaska.edu>
2024-04-03 00:45:54 +0000

    add unwrapping error correction, add mask arg in util vmin vmax func

--- opensarlab_mintpy_recipe_book_env.sh
+++ opensarlab_mintpy_recipe_book_env.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
-env="osl_mintpy"
-local="/home/jovyan/.local"
+env="opensarlab_mintpy_recipe_book"
+local=$1
 env_prefix=$local"/envs/"$env
 python_version=$(conda run -n $env python --version | cut -b 8-10)
 site_packages=$env_prefix"/lib/python"$python_version"/site-packages"
